22db3009343e82fddaf3e6e5080f24ee54b2fd07
ren <luoh2@rpi.edu>
2016-11-14 22:34:43 -0500

    create custom stv mechanism runner script

--- prefpy/mechanismSTVRunner.py
+++ prefpy/mechanismSTVRunner.py
@@ -1,10 +1,42 @@
 import sys
 from prefpy import io
 from prefpy import mechanism
+from .preference import Preference
+from .profile import Profile
 
-inputFile = open(sys.argv[1], 'r')
-candMap, rankMaps, rankMapsCounts, numVoters = io.read_election_file(inputFile)
+def electionFileToProfile(file):
+    inputFile = open(sys.argv[1], 'r')
+    candMap, rankMaps, rankMapsCounts, numVoters = io.read_election_file(inputFile)
 
-io.pp_profile_toscreen(candMap, rankMaps, rankMapsCounts)
+    preferences = []
+    # ranking: [dict<cand,pos in order>, freq of ranking]
+    for ranking in zip(rankMaps,rankMapsCounts):
+        cands = list(ranking[0].keys())
+        wmg = {}
+        for i in range(len(cands)):
+            for j in range(i+1, len(cands)):
+                cand1, cand2 = cands[i], cands[j]
+                if cand1 not in wmg:
+                    wmg[cand1] = {}
+                if cand2 not in wmg:
+                    wmg[cand2] = {}
+                if ranking[0][cand1] < ranking[0][cand2]:
+                    wmg[cand1][cand2] = 1
+                    wmg[cand2][cand1] = -1
+                elif ranking[0][cand1] > ranking[0][cand2]:
+                    wmg[cand1][cand2] = -1
+                    wmg[cand2][cand1] = 1
+                else:
+                    wmg[cand1][cand2] = 0
+                    wmg[cand2][cand1] = 0
+        preferences.append(Preference(wmg, ranking[1]))
+    return Profile(candMap, preferences)
+            
+profile = electionFileToProfile(sys.argv[1])
+stv = mechanism.MechanismSTV()
+print(stv.getWinners(profile))
+print(stv.getCandScoresMap(profile))
 
-#io.pp_result_toscreen(candMap, scores)
+
+# stvMech = mechanism.mechanismSTV()
+# scores = stvMech.getCandScoresMap(
